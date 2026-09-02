56f3eeee024418715fd7602ec2c3108e90c5f27d
MerVik <stepanenkoboris064@gmail.com>
2026-09-02 19:09:03 +0400

    Updates

--- _nuxt/BAWWwh6q.js
+++
@@ -1,1 +0,0 @@
-import{e as s,i as t,s as r,j as a,f as n}from"./r9kT9h2l.js";const f=s({__name:"index",setup(i){const e=t(),{user:o}=r(e);return o.value?a(o.value.nickname):(e.openAuthPrompt(),n()),()=>{}}});export{f as default};
